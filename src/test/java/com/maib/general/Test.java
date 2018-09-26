5a22f91efcd1a681c2187f5f42a07a6b641f907b
weihaibo <weihaibo@bogon>
2018-09-26 10:33:20 +0800

    add test

--- src/test/java/com/maib/general/Test.java
+++ src/test/java/com/maib/general/Test.java
@@ -1,24 +1,11 @@
 package com.maib.general;
 
-import com.maihb.general.config.MainConfigOfAutowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Test {
 
-    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfAutowired.class);
-
     @org.junit.Test
     public void test1(){
-        printBeans(applicationContext);
-        System.out.println("=========================");
-    }
-
-    private void printBeans(AnnotationConfigApplicationContext applicationContext){
-        String[] defitationNames = applicationContext.getBeanDefinitionNames();
-        for (String defitationName : defitationNames) {
-            System.out.println(defitationName);
-        }
-
+        System.out.println(Math.pow(2,8));
     }
 
 }
