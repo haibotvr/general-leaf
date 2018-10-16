777a37687db9c62d0c8d3292af4d2b80c2717f53
weihaibo <weihaibo@bogon>
2018-10-16 09:20:51 +0800

    change blank

--- src/main/java/com/maihb/general/GeneralMainApplication.java
+++ src/main/java/com/maihb/general/GeneralMainApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GeneralMainApplication {
 
-
     //项目启动器
     public static void main(String[] args) {
         SpringApplication.run(GeneralMainApplication.class, args);
